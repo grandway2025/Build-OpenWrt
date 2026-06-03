97fc2f6110d559efc9c97022e1d554d0d170edfc
grandway2025 <zouchanggan@gmail.com>
2026-06-03 12:24:47 +0800

    Update 02-prepare_package.sh

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -155,6 +155,7 @@ git clone https://$github/sbwml/luci-app-mentohust package/new/mentohust
 rm -rf feeds/packages/utils/coremark
 git clone https://$github/sbwml/openwrt_pkgs package/new/custom --depth=1
 rm -rf package/new/custom/ddns-scripts-aliyun
+rm -rf package/new/custom/luci-app-adguardhome
 # coremark - prebuilt with gcc15
 curl -s $mirror/openwrt/patch/coremark/coremark.aarch64-16-threads > package/new/custom/coremark/src/musl/coremark.aarch64
 
@@ -190,4 +191,4 @@ curl -s $mirror/openwrt/patch/sqm-scripts/Makefile > feeds/packages/net/sqm-scri
 # luci-app-adguardhome
 rm -rf feeds/packages/net/adguardhome
 rm -rf feeds/luci/applications/luci-app-adguardhome
-git clone https://$github/grandway2025/luci-app-adguardhome package/new/luci-app-adguardhome --depth=1
+git clone https://$github/zouchanggan/luci-app-adguardhome package/new/luci-app-adguardhome
